7360a14bbc9b5a566b9bafb30653507f302800dc
Marcos Gómez Buceta <m.gomez@sixtema.es>
2023-11-07 10:58:07 +0100

    Inicio de repositorio

---
+++ superset/superset_config.py
@@ -0,0 +1,9 @@
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True,
+}
+ENABLE_PROXY_FIX = True
+SECRET_KEY = "PIT3IXGyp2PvjwY9bSCzPM9Duq6g94dnf9UUxheDGJytXjVRWrFvPvYNjfLAbxyR"
+LANGUAGES = {
+    "en": {"flag": "us", "name": "English"},
+    "es": {"flag": "es", "name": "Español"},
+}
