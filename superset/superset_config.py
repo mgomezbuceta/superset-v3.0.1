f56d53ca26da51fb7dde23eb0ced39d75252de61
Marcos Gómez Buceta <mgomezbuceta@gmail.com>
2023-11-07 11:06:24 +0100

    Update superset_config.py
    Eliminación de SECRET_KEY expuesta

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -2,7 +2,7 @@ FEATURE_FLAGS = {
     "ENABLE_TEMPLATE_PROCESSING": True,
 }
 ENABLE_PROXY_FIX = True
-SECRET_KEY = "PIT3IXGyp2PvjwY9bSCzPM9Duq6g94dnf9UUxheDGJytXjVRWrFvPvYNjfLAbxyR"
+SECRET_KEY = "TU_SECRET_KEY"
 LANGUAGES = {
     "en": {"flag": "us", "name": "English"},
     "es": {"flag": "es", "name": "Español"},
